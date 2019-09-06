a74ed1bab9d277fbb25a823eddda15f2ebf83199
kumaren14 <kumaren.a@gmail.com>
2019-09-06 15:52:35 -0500

    I have created my art gallery file

--- PuttingItTogether.py
+++ PuttingItTogether.py
@@ -79,6 +79,33 @@ turtle.penup()
 turtle.goto(0, 0)
 turtle.right(90)
 
+turtle.goto(200, -250)
+turtle.pendown()
+
+turtle.color("yellow")
+turtle.begin_fill()
+turtle.circle(60)
+turtle.end_fill()
+
+turtle.penup()
+turtle.color("black")
+turtle.goto(175, -175)
+turtle.pendown()
+
+turtle.begin_fill()
+turtle.circle(10)
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(225, -175)
+turtle.pendown()
+
+turtle.begin_fill()
+turtle.circle(10)
+turtle.end_fill()
+
+
+
 
 
 
